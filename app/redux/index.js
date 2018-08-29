2245c8c6ac6a9b49140aaa7addefd068a09c6d2c
Finn Terdal <fterdal@gmail.com>
2018-08-29 16:15:38 -0500

    clean up

--- app/redux/index.js
+++ app/redux/index.js
@@ -1,15 +1,7 @@
 // `combineReducers` is not currently being used...but it should!
-// When you're ready to use it, un-comment the code below!
+// When you're ready to use it, un-comment the line below!
 
 // import {combineReducers} from 'redux'
-
-// import campuses from './campuses'
-// import students from './students'
-
-// const rootReducer = combineReducers({
-//   campuses,
-//   students,
-// })
 
 const initialState = {}
 
