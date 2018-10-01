51076789a96711e02c8779c55fd511437552cb37
Finn Terdal <fterdal@gmail.com>
2018-10-01 16:49:47 -0500

    modifies the seed file and adds the seed file tests to a new integration section

--- tests/tier-01.js
+++ tests/tier-01.js
@@ -1,10 +1,11 @@
-const {expect} = require('chai');
-import enzyme, {shallow, mount} from 'enzyme'
+const { expect } = require('chai');
+import enzyme, { shallow, mount } from 'enzyme'
 import sinon from 'sinon'
 import React from 'react'
 import Adapter from 'enzyme-adapter-react-16'
-import configureMockStore from 'redux-mock-store';
-import thunkMiddleware from 'redux-thunk';
+import configureMockStore from 'redux-mock-store'
+import { Provider } from 'react-redux'
+import thunkMiddleware from 'redux-thunk'
 
 import * as rrd from 'react-router-dom'
 const { MemoryRouter, Link } = rrd
@@ -15,21 +16,21 @@ const initialState = {
   campuses: [],
   students: [],
 }
-let store = mockStore(initialState)
 
 import mockAxios from './mock-axios'
 import { setCampuses, fetchCampuses } from '../app/redux/campuses'
 import { setStudents, fetchStudents } from '../app/redux/students'
 
 import rootReducer from '../app/redux'
-import {createStore} from 'redux'
-// creates test store using real reducer
-let testStore = createStore(rootReducer)
+import { createStore } from 'redux'
+import store from '../app/store'
 
 const app = require('../server')
 const agent = require('supertest')(app)
 
-const {Campus, Student} = require('../server/db')
+const { db } = require('../server/db')
+const { Campus, Student } = require('../server/db')
+const seed = require('../seed')
 
 const adapter = new Adapter()
 enzyme.configure({ adapter })
@@ -38,14 +39,14 @@ import { AllCampuses } from '../app/components/AllCampuses'
 import { AllStudents } from '../app/components/AllStudents'
 import Root from '../app/components/root'
 
-describe('Tier One', () => {
+describe('Tier One: All Campuses and Students', () => {
   describe('Client-side', () => {
     describe('<AllCampuses /> component', () => {
-      xit('renders "No Campuses" if passed an empty array of campuses', () => {
+      it('renders "No Campuses" if passed an empty array of campuses', () => {
         const wrapper = shallow(<AllCampuses campuses={[]} />)
         expect(wrapper.text().toLowerCase()).to.include('no campuses')
       })
-      xit('renders the campuses passed in as props', () => {
+      it('renders the campuses passed in as props', () => {
         const wrapper = shallow(
           <AllCampuses campuses={[
             { id: 1, name: 'Mars Academy', imageUrl: '/images/mars.png' },
@@ -62,11 +63,11 @@ describe('Tier One', () => {
       })
     })
     describe('<AllStudents /> component', () => {
-      xit('renders "No Students" if passed an empty array of students', () => {
+      it('renders "No Students" if passed an empty array of students', () => {
         const wrapper = shallow(<AllStudents students={[]} />)
         expect(wrapper.text().toLowerCase()).to.include('no students')
       })
-      xit('renders the students passed in as props', () => {
+      it('renders the students passed in as props', () => {
         const wrapper = shallow(<AllStudents students={[
           { id: 1, firstName: 'Mae', lastName: 'Jemison' },
           { id: 2, firstName: 'Sally', lastName: 'Ride' },
@@ -75,70 +76,26 @@ describe('Tier One', () => {
         expect(wrapper.text()).to.include('Sally Ride')
       })
     })
-    describe('Navigation', () => {
-      /** In order to test react-router, we need ot hijack the BrowserRouter
-       *  in the root of our app. Sinon allows us to "stub" the BrowserRouter.
-       *  Whenever a component calls BrowserRouter, it'll instead render a
-       *  component that merely renders the children. After the tests are done,
-       *  let's clean up after ourselves by restoring BrowserRouter.
-       */
-      beforeEach(() => {
-        sinon.stub(rrd, 'BrowserRouter').callsFake(({ children }) => {
-          return (<div>{children}</div>)
-        })
-      })
-      afterEach(() => {
-        rrd.BrowserRouter.restore()
-      })
-      xit('renders <AllCampuses /> at /campuses', () => {
-        const wrapper = mount(
-          <MemoryRouter initialEntries={['/campuses']}>
-            <Root />
-          </MemoryRouter>
-        )
-        expect(wrapper.find(AllCampuses)).to.have.length(1)
-        expect(wrapper.find(AllStudents)).to.have.length(0)
-      })
-      xit('renders <AllStudents /> at /students', () => {
-        const wrapper = mount(
-          <MemoryRouter initialEntries={['/students']}>
-            <Root />
-          </MemoryRouter>
-        )
-        expect(wrapper.find(AllCampuses)).to.have.length(0)
-        expect(wrapper.find(AllStudents)).to.have.length(1)
-      })
-      xit('navbar to navigate to home, campuses, students', () => {
-        const wrapper = mount(
-          <MemoryRouter initialEntries={['/']}>
-            <Root />
-          </MemoryRouter>
-        )
-        const nav = wrapper.find('nav')
-        expect(nav).to.have.length(1)
-        const links = nav.find(Link).map(node => node.get(0).props.to)
-        expect(links).to.include.members(['/', '/campuses', '/students'])
-      })
-    })
     describe('Redux', () => {
+      let fakeStore
       beforeEach(() => {
-        store = mockStore(initialState)
+        fakeStore = mockStore(initialState)
       })
       describe('set campuses', () => {
         const campuses = [
           { id: 1, name: 'Mars Academy', imageUrl: '/images/mars.png' },
           { id: 2, name: 'Jupiter Jumpstart', imageUrl: '/images/jupiter.jpeg' }
         ]
-        xit('setCampuses action creator', () => {
+        it('setCampuses action creator', () => {
           expect(setCampuses(campuses)).to.deep.equal({
             type: 'SET_CAMPUSES',
             campuses,
           })
         })
-        xit('fetchCampuses thunk creator', async () => {
+        it('fetchCampuses thunk creator', async () => {
           mockAxios.onGet('/api/campuses').replyOnce(200, campuses)
-          await store.dispatch(fetchCampuses())
-          const actions = store.getActions()
+          await fakeStore.dispatch(fetchCampuses())
+          const actions = fakeStore.getActions()
           expect(actions[0].type).to.equal('SET_CAMPUSES')
           expect(actions[0].campuses).to.deep.equal(campuses)
         })
@@ -148,34 +105,35 @@ describe('Tier One', () => {
           { id: 1, firstName: 'Mae', lastName: 'Jemison' },
           { id: 2, firstName: 'Sally', lastName: 'Ride' },
         ]
-        xit('setStudents action creator', () => {
+        it('setStudents action creator', () => {
           expect(setStudents(students)).to.deep.equal({
             type: 'SET_STUDENTS',
             students,
           })
         })
-        xit('fetchStudents thunk creator', async () => {
+        it('fetchStudents thunk creator', async () => {
           mockAxios.onGet('/api/students').replyOnce(200, students)
-          await store.dispatch(fetchStudents())
-          const actions = store.getActions()
+          await fakeStore.dispatch(fetchStudents())
+          const actions = fakeStore.getActions()
           expect(actions[0].type).to.equal('SET_STUDENTS')
           expect(actions[0].students).to.deep.equal(students)
         })
       })
       describe('reducer', () => {
+        let testStore
         beforeEach(() => {
           testStore = createStore(rootReducer)
         })
-        xit('returns the initial state by default', () => {
+        it('returns the initial state by default', () => {
           expect(testStore.getState().campuses).to.be.an('array')
           expect(testStore.getState().students).to.be.an('array')
         })
-        xit('reduces on SET_CAMPUSES action', () => {
+        it('reduces on SET_CAMPUSES action', () => {
           const campuses = [
             { id: 1, name: 'Mars Academy', imageUrl: '/images/mars.png' },
             { id: 2, name: 'Jupiter Jumpstart', imageUrl: '/images/jupiter.jpeg' }
           ]
-          const action = {type: 'SET_CAMPUSES', campuses}
+          const action = { type: 'SET_CAMPUSES', campuses }
 
           const prevState = testStore.getState()
           testStore.dispatch(action)
@@ -184,12 +142,12 @@ describe('Tier One', () => {
           expect(newState.campuses).to.be.deep.equal(campuses);
           expect(newState.campuses).to.not.be.equal(prevState.campuses);
         })
-        xit('reduces on SET_STUDENTS action', () => {
+        it('reduces on SET_STUDENTS action', () => {
           const students = [
             { id: 1, firstName: 'Mae', lastName: 'Jemison' },
             { id: 2, firstName: 'Sally', lastName: 'Ride' },
           ]
-          const action = {type: 'SET_STUDENTS', students}
+          const action = { type: 'SET_STUDENTS', students }
 
           const prevState = testStore.getState()
           testStore.dispatch(action)
@@ -202,8 +160,8 @@ describe('Tier One', () => {
     })
   })
   describe('API', () => {
-    const {findAll: campusFindAll} = Campus
-    const {findAll: studentFindAll} = Student
+    const { findAll: campusFindAll } = Campus
+    const { findAll: studentFindAll } = Student
     beforeEach(() => {
       Campus.findAll = sinon.spy(() => [
         { id: 1, name: 'Mars Academy', imageUrl: '/images/mars.png' },
@@ -218,7 +176,7 @@ describe('Tier One', () => {
       Campus.findAll = campusFindAll
       Student.findAll = studentFindAll
     })
-    xit('GET /api/campuses responds with all campuses', async () => {
+    it('GET /api/campuses responds with all campuses', async () => {
       const response = await agent
         .get('/api/campuses')
         .expect(200)
@@ -228,7 +186,7 @@ describe('Tier One', () => {
       ])
       expect(Campus.findAll.calledOnce).to.be.equal(true)
     })
-    xit('GET /api/students responds with all students', async () => {
+    it('GET /api/students responds with all students', async () => {
       const response = await agent
         .get('/api/students')
         .expect(200)
@@ -241,7 +199,7 @@ describe('Tier One', () => {
   })
   describe('Models', () => {
     describe('Campus', () => {
-      xit('has fields name, address, imageUrl, description', () => {
+      it('has fields name, address, imageUrl, description', () => {
         const campus = Campus.build({
           name: 'Jupiter Jumpstart',
           address: '5.2 AU',
@@ -253,7 +211,7 @@ describe('Tier One', () => {
         expect(campus.imageUrl).to.equal('/images/jupiter.png')
         expect(campus.description).to.equal('The best JavaScript Academy for toddlers in the solar system!')
       })
-      xit('requires name and address', async () => {
+      it('requires name and address', async () => {
         const campus = Campus.build()
         try {
           await campus.validate()
@@ -264,7 +222,7 @@ describe('Tier One', () => {
           expect(err.message).to.contain('address cannot be null')
         }
       })
-      xit('name and address cannot be empty', async () => {
+      it('name and address cannot be empty', async () => {
         const campus = Campus.build({ name: '', address: '' })
         try {
           await campus.validate()
@@ -275,7 +233,7 @@ describe('Tier One', () => {
           expect(err.message).to.contain('Validation notEmpty on address')
         }
       })
-      xit('default imageUrl if left blank', async () => {
+      it('default imageUrl if left blank', async () => {
         const campus = Campus.build({
           name: 'Jupiter Jumpstart',
           address: '5.2 AU',
@@ -286,8 +244,9 @@ describe('Tier One', () => {
       })
     })
     describe('Student', () => {
-      xit('has fields firstName, lastName, email, imageUrl, gpa', () => {
-        const student = Student.build({
+      afterEach(() => db.sync({ force: true }))
+      it('has fields firstName, lastName, email, imageUrl, gpa', async () => {
+        const student = await Student.create({
           firstName: 'Sally',
           lastName: 'Ride',
           email: 'sallyride@nasa.gov',
@@ -298,9 +257,9 @@ describe('Tier One', () => {
         expect(student.lastName).to.equal('Ride')
         expect(student.imageUrl).to.equal('/images/sallyride.png')
         expect(student.email).to.equal('sallyride@nasa.gov')
-        expect(student.gpa).to.equal(3.8)
+        expect(parseFloat(student.gpa)).to.equal(3.8)
       })
-      xit('requires firstName, lastName, email', async () => {
+      it('requires firstName, lastName, email', async () => {
         const student = Student.build()
         try {
           await student.validate()
@@ -312,7 +271,7 @@ describe('Tier One', () => {
           expect(err.message).to.contain('email cannot be null')
         }
       })
-      xit('firstName, lastName, email cannot be empty', async () => {
+      it('firstName, lastName, email cannot be empty', async () => {
         const student = Student.build({ firstName: '', lastName: '', email: '' })
         try {
           await student.validate()
@@ -324,7 +283,7 @@ describe('Tier One', () => {
           expect(err.message).to.contain('Validation notEmpty on email')
         }
       })
-      xit('email must be a valid email', async () => {
+      it('email must be a valid email', async () => {
         const student = Student.build({
           firstName: 'Sally',
           lastName: 'Ride',
@@ -339,7 +298,7 @@ describe('Tier One', () => {
           expect(err.message).to.contain('Validation isEmail on email')
         }
       })
-      xit('gpa must be decimal between 0.0 and 4.0', async () => {
+      it('gpa must be a float between 0.0 and 4.0', async () => {
         const student = {
           firstName: 'Sally',
           lastName: 'Ride',
@@ -348,7 +307,7 @@ describe('Tier One', () => {
         }
         const overachiever = Student.build(student)
         try {
-          await overachiever.validate()
+          await overachiever.save()
           throw Error('validation should have failed with too high gpa')
         }
         catch (err) {
@@ -364,7 +323,7 @@ describe('Tier One', () => {
           expect(err.message).to.contain('Validation min on gpa')
         }
       })
-      xit('default imageUrl if left blank', () => {
+      it('default imageUrl if left blank', () => {
         const student = Student.build({ firstName: '', lastName: '', email: '' })
         expect(student.imageUrl).to.be.a('string')
         expect(student.imageUrl.length).to.be.greaterThan(1)
@@ -374,32 +333,117 @@ describe('Tier One', () => {
       let student1, student2, campus
       beforeEach(async () => {
         campus = await Campus.create({
-          id: 1,
           name: 'Jupiter Jumpstart',
           address: '5.2 AU',
         })
         student1 = await Student.create({
-          id: 1,
           firstName: 'Sally',
           lastName: 'Ride',
           email: 'sallyride@nasa.gov',
-          campusId: 1,
+          campusId: campus.id,
         })
         student2 = await Student.create({
-          id: 2,
           firstName: 'Mae',
           lastName: 'Jemison',
           email: 'maejemison@nasa.gov',
-          campusId: 1,
+          campusId: campus.id,
         })
       })
-      xit('a student may be assigned to at most one campus', async () => {
+      afterEach(() => db.sync({ force: true }))
+      it('a student may be assigned to at most one campus', async () => {
         const sallysCampus = await student1.getCampus()
         expect(sallysCampus.name).to.equal(campus.name)
       })
-      xit('a campus may have many enrolled students', async () => {
+      it('a campus may have many enrolled students', async () => {
         const result = await campus.hasStudents([student1, student2])
         expect(result).to.be.equal(true)
+      })
+    })
+  })
+  describe('Integration', () => {
+    describe('Navigation', () => {
+      /** In order to test react-router, we need to hijack the BrowserRouter
+       *  in the root of our app. Sinon allows us to "stub" the BrowserRouter.
+       *  Whenever a component calls BrowserRouter, it'll instead render a
+       *  component that merely renders the children. After the tests are done,
+       *  let's clean up after ourselves by restoring BrowserRouter.
+       */
+      beforeEach(() => {
+        sinon.stub(rrd, 'BrowserRouter').callsFake(({ children }) => {
+          return (<div>{children}</div>)
+        })
+        const campuses = [
+          { id: 1, name: 'Mars Academy', imageUrl: '/images/mars.png' },
+          { id: 2, name: 'Jupiter Jumpstart', imageUrl: '/images/jupiter.jpeg' },
+        ];
+        const students = [
+          { id: 1, firstName: 'Mae', lastName: 'Jemison' },
+          { id: 2, firstName: 'Sally', lastName: 'Ride' },
+        ];
+        mockAxios.onGet('/api/campuses').replyOnce(200, campuses);
+        mockAxios.onGet('/api/students').replyOnce(200, students);
+      })
+      afterEach(() => {
+        rrd.BrowserRouter.restore()
+      })
+      it('renders <AllCampuses /> at /campuses', async () => {
+        const wrapper = mount(
+          <Provider store={store}>
+            <MemoryRouter initialEntries={['/campuses']}>
+              <Root />
+            </MemoryRouter>
+          </Provider>
+        )
+        await store.nextDispatch()
+        expect(wrapper.find(AllCampuses)).to.have.length(1)
+        expect(wrapper.find(AllStudents)).to.have.length(0)
+      })
+      it('renders <AllStudents /> at /students', () => {
+        const wrapper = mount(
+          <Provider store={store}>
+            <MemoryRouter initialEntries={['/students']}>
+              <Root />
+            </MemoryRouter>
+          </Provider>
+        )
+        expect(wrapper.find(AllCampuses)).to.have.length(0)
+        expect(wrapper.find(AllStudents)).to.have.length(1)
+      })
+      it('navbar to navigate to home, campuses, students', () => {
+        const wrapper = mount(
+          <Provider store={store}>
+            <MemoryRouter initialEntries={['/']}>
+              <Root />
+            </MemoryRouter>
+          </Provider>
+        )
+        const nav = wrapper.find('nav')
+        expect(nav).to.have.length(1)
+        const links = nav.find(Link).map(node => node.get(0).props.to)
+        expect(links).to.include.members(['/', '/campuses', '/students'])
+      })
+    })
+    describe('Seed file', () => {
+      before(seed)
+      it('populates the database with at least three campuses', async () => {
+        const campuses = await Campus.findAll()
+        expect(campuses).to.have.lengthOf.at.least(3)
+      })
+      it('populates the database with at least four students', async () => {
+        const students = await Student.findAll()
+        expect(students).to.have.lengthOf.at.least(4)
+      })
+      it('creates exactly one campus has no students', async () => {
+        const campuses = await Campus.findAll({
+          include: [{ model: Student }]
+        })
+        const emptyCampuses = campuses.filter(campus => !campus.students.length)
+        expect(emptyCampuses).to.have.lengthOf(1)
+      })
+      it('creates exactly one student is not enrolled in a campus', async () => {
+        const students = await Student.findAll()
+        const notEnrolled = students.filter(student => !student.campusId)
+        expect(notEnrolled).to.have.lengthOf(1)
       })
     })
   })
