8f8d2540dafc03763e4f751843e78b2d947956ce
Finn Terdal <fterdal@gmail.com>
2018-11-21 10:52:11 -0600

    pull tests 01 robots from runthrough branch

--- tests/tier-01/01-robots.js
+++ tests/tier-01/01-robots.js
@@ -6,7 +6,8 @@ import Adapter from 'enzyme-adapter-react-16.3'
 import configureMockStore from 'redux-mock-store'
 import thunkMiddleware from 'redux-thunk'
 import { Provider } from 'react-redux'
-import { MemoryRouter } from 'react-router-dom'
+import * as rrd from 'react-router-dom'
+const { MemoryRouter, Link } = rrd
 
 const middlewares = [thunkMiddleware]
 const mockStore = configureMockStore(middlewares)
@@ -19,27 +20,39 @@ import { setRobots, fetchRobots } from '../../app/redux/robots'
 
 import appReducer from '../../app/redux'
 import { createStore } from 'redux'
+import store from '../../app/store'
 
 const app = require('../../server')
 const agent = require('supertest')(app)
 
 const { db } = require('../../server/db')
 const { Robot } = require('../../server/db')
+const seed = require('../../seed')
 
 const adapter = new Adapter()
 enzyme.configure({ adapter })
 
-import { AllRobots } from '../../app/components/AllRobots'
+import ConnectedAllRobots, { AllRobots } from '../../app/components/AllRobots'
+// import NavBar from '../../app/components/NavBar' // TODO: delete this
+import Root from '../../app/components/root'
 
-describe('Tier One: Robots', () => {
+// Sometimes, we want to wait for a short time for async events to finish.
+const waitFor = (wait) =>
+  new Promise((resolve) => setTimeout(resolve, wait))
+
+describe.only('Tier One: Robots', () => {
   let fakeStore
+  const robots = [
+    { id: 1, name: 'R2-D2', imageUrl: '/images/r2d2.png' },
+    { id: 2, name: 'WALL-E', imageUrl: '/images/walle.jpeg' },
+  ]
   beforeEach(() => {
     fakeStore = mockStore(initialState)
   })
 
   describe('<AllRobots /> component', () => {
 
-    xit('renders the robots passed in as props', () => {
+    it('renders the robots passed in as props', () => {
       const wrapper = mount(
         <Provider store={fakeStore}>
           <MemoryRouter>
@@ -59,26 +72,65 @@ describe('Tier One: Robots', () => {
       ])
     })
 
-    xit('*** renders "No Robots" if passed an empty array of robots', () => {
-      throw new Error('replace this error with your own test')
+    it('*** renders "No Robots" if passed an empty array of robots', () => {
+      const wrapper = mount(
+        <Provider store={fakeStore}>
+          <MemoryRouter>
+            <AllRobots robots={[]} />
+          </MemoryRouter>
+        </Provider>
+      )
+      expect(wrapper.text().toLowerCase()).to.include('no robots')
     })
+  })
+
+  describe('Navigation', () => {
+    beforeEach(() => {
+      sinon.stub(rrd, 'BrowserRouter').callsFake(({ children }) => (
+        <div>{children}</div>
+      ))
+      mockAxios.onGet('/api/robots').replyOnce(200, robots)
+    })
+    afterEach(() => {
+      rrd.BrowserRouter.restore()
+    })
+
+    it('renders <AllRobots /> at path /robots', () => {
+      const wrapper = mount(
+        <Provider store={fakeStore}>
+          <MemoryRouter initialEntries={['/robots']}>
+            <Root />
+          </MemoryRouter>
+        </Provider>
+      )
+      expect(wrapper.find(AllRobots)).to.have.length(1)
+    })
+
+    it('*** navbar has links to "/robots" and "/" (homepage)', () => {
+      throw new Error('replace this error with your own test')
+      // const wrapper = mount(
+      //   <Provider store={fakeStore}>
+      //     <MemoryRouter>
+      //       <NavBar />
+      //     </MemoryRouter>
+      //   </Provider>
+      // )
+      // const navLinks = wrapper.find(Link).map(node => node.get(0).props.to)
+      // expect(navLinks).to.include.members(['/', '/robots'])
+    })
+
   })
 
   describe('Redux', () => {
     describe('set/fetch robots', () => {
-      const robots = [
-        { id: 1, name: 'R2-D2', imageUrl: '/images/r2d2.png' },
-        { id: 2, name: 'WALL-E', imageUrl: '/images/walle.jpeg' },
-      ]
-
-      xit('setRobots action creator', () => {
+      it('setRobots action creator', () => {
         expect(setRobots(robots)).to.deep.equal({
           type: 'SET_ROBOTS',
           robots,
         })
       })
 
-      xit('fetchRobots thunk creator', async () => {
+      it('fetchRobots thunk creator', async () => {
         mockAxios.onGet('/api/robots').replyOnce(200, robots)
         await fakeStore.dispatch(fetchRobots())
         const actions = fakeStore.getActions()
@@ -93,15 +145,11 @@ describe('Tier One: Robots', () => {
         testStore = createStore(appReducer)
       })
 
-      xit('*** returns the initial state by default', () => {
-        throw new Error('replace this error with your own test')
+      it('*** returns the initial state by default', () => {
+        expect(testStore.getState()).to.deep.equal(initialState)
       })
 
-      xit('reduces on SET_ROBOTS action', () => {
-        const robots = [
-          { id: 1, name: 'R2-D2', imageUrl: '/images/r2d2.png' },
-          { id: 2, name: 'WALL-E', imageUrl: '/images/walle.jpeg' },
-        ]
+      it('reduces on SET_ROBOTS action', () => {
         const action = { type: 'SET_ROBOTS', robots }
 
         const prevState = testStore.getState()
@@ -112,6 +160,45 @@ describe('Tier One: Robots', () => {
         expect(newState.robots).to.not.be.equal(prevState.robots);
       })
     })
+
+    describe('react-redux', () => {
+      beforeEach(() => {
+        mockAxios.onGet('/api/robots').replyOnce(200, robots)
+      })
+
+      it('initializes robots from the server when the app first loads', async () => {
+        const reduxStateBeforeMount = store.getState()
+        expect(reduxStateBeforeMount.robots).to.deep.equal([])
+        mount(
+          <Provider store={store}>
+            <MemoryRouter initialEntries={['/']}>
+              <Root />
+            </MemoryRouter>
+          </Provider>
+        )
+        await waitFor(10) // wait for 10 milliseconds
+        const reduxStateAfterMount = store.getState()
+        expect(reduxStateAfterMount.robots).to.deep.equal(robots)
+      })
+
+      it('<AllRobots /> is passed robots from store as props', async () => {
+        const wrapper = mount(
+          <Provider store={store}>
+            <MemoryRouter initialEntries={['/robots']}>
+              <ConnectedAllRobots />
+            </MemoryRouter>
+          </Provider>
+        )
+        store.dispatch(fetchRobots()) // fetch the robots
+        await waitFor(10) // wait for 10 milliseconds
+        wrapper.update() // forces the component to re-render airbnb.io/enzyme/docs/api/ShallowWrapper/update.html
+        const { robots: reduxRobotes } = store.getState()
+        const { robots: componentRobots } = wrapper.find(AllRobots).props()
+        expect(componentRobots).to.deep.equal(reduxRobotes)
+      })
+    })
+
+
   })
 
   describe('Express API', () => {
@@ -127,16 +214,16 @@ describe('Tier One: Robots', () => {
       { id: 2, name: 'WALL-E', imageUrl: '/images/walle.jpeg' },
     ])
     beforeEach(() => {
-      if (Robot && Robot.findall) sinon.replace(Robot, 'findAll', fakeFindAll)
+      sinon.replace(Robot, 'findAll', fakeFindAll)
     })
     afterEach(() => {
       sinon.restore()
     })
 
-    xit('GET /api/robots responds with all robots', async () => {
+    it('GET /api/robots responds with all robots', async () => {
       const response = await agent
         .get('/api/robots')
-        .timeout({ deadline: 50 })
+        .timeout({ deadline: 20 })
         .expect(200)
       expect(response.body).to.deep.equal([
         { id: 1, name: 'R2-D2', imageUrl: '/images/r2d2.png' },
@@ -145,15 +232,15 @@ describe('Tier One: Robots', () => {
       expect(Robot.findAll.calledOnce).to.be.equal(true)
     })
 
-    xit('GET /api/robots responds with error 500 when database throws error', async () => {
+    it('GET /api/robots responds with error 500 when database throws error', async () => {
       sinon.restore()
       const fakeFindAllWithError = sinon.fake.rejects(
         Error('Ooopsies, the database is on fire!')
       )
-      if (Robot && Robot.findall) sinon.replace(Robot, 'findAll', fakeFindAllWithError)
+      sinon.replace(Robot, 'findAll', fakeFindAllWithError)
       await agent
         .get('/api/robots')
-        .timeout({ deadline: 50 })
+        .timeout({ deadline: 20 })
         .expect(500)
       expect(Robot.findAll.calledOnce).to.be.equal(true)
     })
@@ -172,7 +259,7 @@ describe('Tier One: Robots', () => {
     })
     afterEach(() => db.sync({ force: true }))
 
-    xit('has fields name, imageUrl, fuelType, fuelLevel', async () => {
+    it('has fields name, imageUrl, fuelType, fuelLevel', async () => {
       robot.notARealAttribute = 'does not compute'
       const savedRobot = await Robot.create(robot)
       expect(savedRobot.name).to.equal('R2-D2')
@@ -182,11 +269,25 @@ describe('Tier One: Robots', () => {
       expect(savedRobot.notARealAttribute).to.equal(undefined)
     })
 
-    xit('*** name cannot be null or an empty string', () => {
-      throw new Error('replace this error with your own test')
+    it('*** name cannot be null or an empty string', async () => {
+      // throw new Error('replace this error with your own test')
+      try {
+        robot.name = null
+        const nullNameRobot = await Robot.create(robot)
+        if (nullNameRobot) throw Error('Validation should have failed with null name')
+      } catch (err) {
+        expect(err.message).to.not.have.string('Validation should have failed')
+      }
+      try {
+        robot.name = ''
+        const emptyNameRobot = await Robot.create(robot)
+        if (emptyNameRobot) throw Error('Validation should have failed with empty name')
+      } catch (err) {
+        expect(err.message).to.not.have.string('Validation should have failed')
+      }
     })
 
-    xit('fuelType can only be gas, diesel, or electric (defaults to electric)', async () => {
+    it('fuelType can only be gas, diesel, or electric (defaults to electric)', async () => {
       robot.fuelType = 'the power of love'
       try {
         const badFuelRobot = await Robot.create(robot)
@@ -199,7 +300,7 @@ describe('Tier One: Robots', () => {
       expect(defaultFuelRobot.fuelType).to.equal('electric')
     })
 
-    xit('fuelLevel must be between 0 and 100 (defaults to 100)', async () => {
+    it('fuelLevel must be between 0 and 100 (defaults to 100)', async () => {
       robot.fuelLevel = -10
       try {
         const negativeFuelRobot = await Robot.create(robot)
@@ -217,6 +318,13 @@ describe('Tier One: Robots', () => {
       delete robot.fuelLevel
       const defaultFuelLevelRobot = await Robot.create(robot)
       expect(defaultFuelLevelRobot.fuelLevel).to.equal(100)
+    })
+  })
+  describe('Seed File', () => {
+    beforeEach(seed)
+    it('populates the database with at least three robots', async () => {
+      const seedRobots = await Robot.findAll()
+      expect(seedRobots).to.have.lengthOf.at.least(3)
     })
   })
 })
