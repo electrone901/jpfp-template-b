205c67754c72b75b3c237789b29e0aea48f6a5b8
Finn <finnterdal@gmail.com>
2019-10-16 10:23:42 -0500

    observed a bug where the RobotForm (Edit) loads the previous robot data

--- app/components/RobotForm.js
+++ app/components/RobotForm.js
@@ -3,26 +3,42 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { postRobot, putRobot } from '../redux'
 
+const blankState = {
+  name: '',
+  fuelType: '',
+  fuelLevel: '100',
+  imageUrl: '',
+}
+
 // This form can be used to create a new robot or edit an existing
 // one. If passed a robot, it pre-populates the form. Otherwise,
 // it creates a new robot.
 export class RobotForm extends React.Component {
-  state = {
-    name: '',
-    fuelType: '',
-    fuelLevel: '100',
-    imageUrl: '',
+  constructor(props) {
+    super(props)
+    console.log('PROPS', props)
+    if (props.robotToEdit) {
+      this.state = { ...props.robotToEdit }
+    } else {
+      this.state = blankState
+    }
   }
-  handleSubmit = (event) => {
+  handleSubmit = event => {
     event.preventDefault()
     const createOrEdit = this.props.robotToEdit
       ? this.props.editRobot
       : this.props.createRobot
     createOrEdit(this.state)
+    if (!this.props.projectToEdit) {
+      this.setState(blankState)
+    }
   }
-  static getDerivedStateFromProps(nextProps) {
-    return nextProps.robotToEdit
+  componentWillUnmount() {
+    console.log('ROBOT FORM WILL UNMOUNT')
   }
+  // static getDerivedStateFromProps(nextProps) {
+  //   return nextProps.robotToEdit
+  // }
   handleChange = ({ target: { name, value } }) => {
     this.setState({ [name]: value })
   }
@@ -30,9 +46,7 @@ export class RobotForm extends React.Component {
     const { name, fuelType, fuelLevel, imageUrl } = this.state
     return (
       <div>
-        <form
-          onSubmit={this.handleSubmit}>
-
+        <form onSubmit={this.handleSubmit}>
           <div className="labelInput">
             <label htmlFor="name">Name: </label>
             <input
@@ -40,12 +54,17 @@ export class RobotForm extends React.Component {
               name="name"
               type="text"
               value={name}
-              onChange={this.handleChange} />
+              onChange={this.handleChange}
+            />
           </div>
 
           <div className="labelInput">
             <label htmlFor="fuelType">Fuel Type: </label>
-            <select name="fuelType" value={fuelType} onChange={this.handleChange}>
+            <select
+              name="fuelType"
+              value={fuelType}
+              onChange={this.handleChange}
+            >
               <option value="electric">Electric</option>
               <option value="gas">Gas</option>
               <option value="diesel">Diesel</option>
@@ -58,7 +77,8 @@ export class RobotForm extends React.Component {
               name="fuelLevel"
               type="number"
               value={fuelLevel}
-              onChange={this.handleChange} />
+              onChange={this.handleChange}
+            />
           </div>
 
           <div className="labelInput">
@@ -67,11 +87,13 @@ export class RobotForm extends React.Component {
               name="imageUrl"
               type="text"
               value={imageUrl}
-              onChange={this.handleChange} />
+              onChange={this.handleChange}
+            />
           </div>
 
-          <button className="submitButton" type="submit">Submit</button>
-
+          <button className="submitButton" type="submit">
+            Submit
+          </button>
         </form>
       </div>
     )
@@ -79,16 +101,20 @@ export class RobotForm extends React.Component {
 }
 
 const mapState = ({ robot }, ownProps) => {
+  console.log('robot', robot)
   return {
-    robotToEdit: ownProps.match.params.id ? robot : null
+    robotToEdit: ownProps.match.params.id ? robot : null,
   }
 }
 
-const mapDispatch = (dispatch) => ({
-  createRobot: (robot) => dispatch(postRobot(robot)),
-  editRobot: (robot) => dispatch(putRobot(robot)),
+const mapDispatch = dispatch => ({
+  createRobot: robot => dispatch(postRobot(robot)),
+  editRobot: robot => dispatch(putRobot(robot)),
 })
 
 export default withRouter(
-  connect(mapState, mapDispatch)(RobotForm)
+  connect(
+    mapState,
+    mapDispatch
+  )(RobotForm)
 )
